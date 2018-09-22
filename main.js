29494c5302100c60ae499a06f5f722ad18101525
Colin White <clwhite814@gmail.com>
2018-09-21 19:49:50 -0500

    the user input is being split into an array

--- main.js
+++ main.js
@@ -1,1 +1,45 @@
 console.log('Hello World');
+
+const swedish = {
+    "merry":"god", 
+    "christmas":"jul", 
+    "and":"och", 
+    "happy":"gott", 
+    "new":"nytt", 
+    "year":"år"
+}
+
+const french = {
+    "merry":"joyeux", 
+    "christmas":"Noël", 
+    "and":"et", 
+    "happy":"content", 
+    "new":"Nouveau", 
+    "year":"an"
+}
+
+const spanish = {
+    "merry":"alegre", 
+    "christmas":"Navidad", 
+    "and":"y", 
+    "happy":"contento", 
+    "new":"nuevo", 
+    "year":"año"
+}
+
+const userMessage = () => {
+    const input = document.getElementById('userInput');
+    return input.value;
+};
+
+const translate1 = (event) => {
+    let test = userMessage().split(' ');
+    console.log(test);
+};
+
+const swedishButton = document.getElementById('lan1');
+swedishButton.addEventListener('click', translate1);
+
+
+
+
