198e91d78fc833db118e16c72685e5efa7f95e59
Colin White <clwhite814@gmail.com>
2018-09-24 08:21:24 -0500

    WIP - spaces between translated string

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-console.log('Hello World');
+const en = {
+    "merry":"merry", 
+    "christmas":"christmas", 
+    "and":"and", 
+    "happy":"happy", 
+    "new":"new", 
+    "year":"year"
+}
 
 const swedish = {
     "merry":"god", 
@@ -27,17 +34,33 @@ const spanish = {
     "year":"año"
 }
 
+let englishChecker = Object.keys(en);
+
+const printToDom = (stringToPrint, divId) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML += stringToPrint;
+};
+
 const userMessage = () => {
-    const input = document.getElementById('userInput');
-    return input.value;
+    let inputDiv = document.getElementById('userInput');
+    let inputVal = inputDiv.value;
+    let inputLC = inputVal.toLowerCase();
+    let inputArray = inputLC.split(' ');
+    return inputArray;
 };
 
-const translate1 = (event) => {
-    let test = userMessage().split(' ');
-    console.log(test);
+const translate1 = () => {
+    let swedStringBuilder = '';
+    const userArray = userMessage();
+    for (let i = 0; i < userArray.length; i++) {
+        let userChecker = userArray[i];      
+        console.log(swedish[userChecker]);
+        swedStringBuilder += `${swedish[userChecker]} `;
+}
+printToDom(swedStringBuilder, 'userOutput');
 };
 
-const swedishButton = document.getElementById('lan1');
+const swedishButton = document.getElementById('swedButton');
 swedishButton.addEventListener('click', translate1);
 
 
